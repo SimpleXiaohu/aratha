0b6e8d6d22a22342701d9da51d5aed8bfe83371a
SimpleXiaohu <1048448520@qq.com>
2024-03-25 20:04:42 +0800

    Format analysis js file

--- string_test/test.js
+++ string_test/test.js
@@ -1,9 +1,45 @@
-var value1 = J$.readString();
-var value2 = J$.readString();
-function escapeAttribute(value1,value2) {
-    if(/\"/.test(value1)){
-        console.log(0);
-    }else if(value1 === value2)
-        console.log(1);
+function parse(str) {
+    var match = /^((?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|years?|yrs?|y)?$/i.exec(str);
+    if (!match) return;
+    var n = parseFloat(match[1]);
+    var type = (match[2] || 'ms').toLowerCase();
+    switch (type) {
+        case 'years':
+        case 'year':
+        case 'yrs':
+        case 'yr':
+        case 'y':
+            return n * y;
+        case 'days':
+        case 'day':
+        case 'd':
+            return n * d;
+        case 'hours':
+        case 'hour':
+        case 'hrs':
+        case 'hr':
+        case 'h':
+            return n * h;
+        case 'minutes':
+        case 'minute':
+        case 'mins':
+        case 'min':
+        case 'm':
+            return n * m;
+        case 'seconds':
+        case 'second':
+        case 'secs':
+        case 'sec':
+        case 's':
+            return n * s;
+        case 'milliseconds':
+        case 'millisecond':
+        case 'msecs':
+        case 'msec':
+        case 'ms':
+            return n;
+    }
 }
-escapeAttribute(value1,value2);
+
+var str = J$.readString()
+parse(str)
