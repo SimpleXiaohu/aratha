0e0fecabcafded36886cfe2bbbf8aeef808d0ae0
SimpleXiaohu <1048448520@qq.com>
2024-06-24 17:16:57 +0800

    add toolpath config

--- importToolPath.sh
+++ importToolPath.sh
@@ -3,19 +3,20 @@ tPath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cat << EOF > "$tPath/toolconfig.json"
 {
-  "Binaries" : {
-		"z3" : {
-			"path" : "$tPath/solverbin/z3"
-		},
-		"cvc5" : {
-				"path" : "$tPath/solverbin/cvc5"
-		},
-		"ostrich" : {
-				"path" : "$tPath/../ostrich/ostrich"
-		},
-		"ostrichcea" : {
-				"path" : "$tPath/../ostrich/ostrichCEA"
-		}
+  "z3" : {
+    "path" : "$tPath/solverbin/z3/z3"
+  },
+  "cvc5" : {
+      "path" : "$tPath/solverbin/cvc5/cvc5"
+  },
+  "z3str" : {
+      "path" : "$tPath/solverbin/z3str/z3"
+  },
+  "ostrich" : {
+      "path" : "$tPath/../ostrich/ostrich"
+  },
+  "ostrichcea" : {
+      "path" : "$tPath/../ostrich/ostrichCEA"
   }
 }
 EOF
