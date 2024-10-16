490953786987da747a783728d8bf1596b3d97ff2
SuperMaxine <2586540653@qq.com>
2024-10-16 13:59:23 +0800

    解决了无法遇到正则的问题

--- string_test/minicase.js
+++ string_test/minicase.js
@@ -1,6 +1,10 @@
 function main (str) {
     if (str.length > 10 && str.length < 50 && str.substring(8, 14) === 'github') {
-        var matched = str.match(/https:\/\/(.*\/?)*\//i)
+        // var matched = str.match(/https:\/\/(.*\/?)*\//i)
+        var matched = /https:\/\/(.*\/?)*\//i.exec(str)
+        if (matched) {
+            console.log(matched[0])
+        }
         console.log(matched)
     }
 }
