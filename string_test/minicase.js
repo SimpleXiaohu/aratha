d6d7650beca9cd230cb86a624a2c39595a107252
SuperMaxine <2586540653@qq.com>
2024-10-16 13:29:06 +0800

    添加minicase例子，之前的提交记录丢了忘了怎么修复不生成正则约束了

--- string_test/minicase.js
+++ string_test/minicase.js
@@ -1,6 +1,6 @@
 function main (str) {
     if (str.length > 10 && str.length < 50 && str.substring(8, 14) === 'github') {
-        var matched = str.match(/https:\/\/(.*\/?)*/i)
+        var matched = str.match(/https:\/\/(.*\/?)*\//i)
         console.log(matched)
     }
 }
